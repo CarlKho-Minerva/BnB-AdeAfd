69f3f0f84e3d1852b6047a52ce999e45efb8fef8
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-03-23 07:21:25 +0100

    barebones

--- src/main.js
+++ src/main.js
@@ -41,14 +41,16 @@ await app.load('https://prod.spline.design/ppsh64LilSrfESli/scene.splinecode');
 window.app = app;
 
 
-// NippleJS - Joystick connection
-const joystick = nipplejs.create();
-joystick.on("move", (e, data) => {
-  myPlayer().setState("dir", data.vector);
-});
-joystick.on("end", () => {
-  myPlayer().setState("dir", {x:0, y:0});
-});
+// // NippleJS - Joystick connection
+// const joystick = nipplejs.create();
+// joystick.on("move", (e, data) => {
+//   myPlayer().setState("dir", data.vector);
+// });
+// joystick.on("end", () => {
+//   myPlayer().setState("dir", {x:0, y:0});
+// });
+
+// Spline API
 
 let players = [];
 function getObjectFromSpline(name){
@@ -60,26 +62,26 @@ onPlayerJoin((state) => {
   players.push({state, camera});
 });
 
-// instantiate class AnimationFrame
-const loop = new AnimationFrame(120, () => {
-  if (isHost()){
-    for (const player of players) {
-      const controls = player.state.getState("dir") || {};
-      player.ball.rigidBody.setLinvel({ 
-        x: controls?.x * 4.0 || 0.0,
-        y:player.ball.rigidBody.linvel().y, 
-        z: controls?.y * -4.0 || 0.0
-      }, true);
-      player.state.setState("pos", player.ball.rigidBody.translation());
-    }
-  }
-  else{
-    for (const player of players) {
-      const pos = player.state.getState("pos");
-      if (pos){
-        player.ball.rigidBody.setTranslation(pos, true);
-      }
-    }
-  }
-});
-loop.start();
+// // instantiate class AnimationFrame
+// const loop = new AnimationFrame(120, () => {
+//   if (isHost()){
+//     for (const player of players) {
+//       const controls = player.state.getState("dir") || {};
+//       player.ball.rigidBody.setLinvel({ 
+//         x: controls?.x * 4.0 || 0.0,
+//         y:player.ball.rigidBody.linvel().y, 
+//         z: controls?.y * -4.0 || 0.0
+//       }, true);
+//       player.state.setState("pos", player.ball.rigidBody.translation());
+//     }
+//   }
+//   else{
+//     for (const player of players) {
+//       const pos = player.state.getState("pos");
+//       if (pos){
+//         player.ball.rigidBody.setTranslation(pos, true);
+//       }
+//     }
+//   }
+// });
+// loop.start();
